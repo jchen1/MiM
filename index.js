87c40af90eaff466d28dbe3ac170d2271a73c03a
Jeff Chen <jeffrey@cmu.edu>
2012-09-29 20:13:38 -0400

    Now pulls only URLS

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
+var tumblr = require("./tumblr");
 
 var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
-handle["/pull"] = requestHandlers.pull;
+handle["/pull"] = tumblr.pull;
 
 server.start(router.route, handle);
