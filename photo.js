5bc2dea46ba36ff4a13e80e057420bd277fdb18e
Jeff Chen <jeffrey@cmu.edu>
2012-09-30 02:09:48 -0400

    Added (nonfunctional) code to start from URL

--- photo.js
+++ photo.js
@@ -2,22 +2,65 @@ var key = "KloQFw7b0NkGEPsbHPBdlnz2VbA76Zmo555EghQ4ru2PpizJqV";
 var http = require("http");
 var util = require("util");
 var events = require("events");
+var url = require("url");
+var querystring = require("querystring");
 
 var Photo = function() {
 	var self = this;
 	events.EventEmitter.call(self);
 
 	self.initTagged = function(tag) {
-		var photo = {tag:tag, timestamp:0, tags:[], url:'', post_url:'', similar:[]};
+		var photo = {tag:tag, data:[], timestamp:0, tags:[], url:'', post_url:'', similar:[]};
 		console.log("newTaggedPhoto");
 		self.emit("newTaggedPhoto", photo, 1);
 	}
+/*
+	self.initURL = function(link) {
+		var photo = {tag:'', timestamp:0, tags:[], url:'', post_url:link, similar:[]};
 
-	self.initURL = function(url) {
-		var photo = {tag:'', timestamp:0, tags:[], url:'', post_url:url, similar:[]};
+		var host = photo.post_url.replace('http://', '').split('/')[0];
+		var pathName = url.parse(photo.post_url).pathname;
+		var id = pathName.split('/')[2];;
+
+		console.log(host);
+
+		var options = {
+			host: 'api.tumblr.com',
+			path: 'v2/blog/' + host + '/posts?id=' + id + '&api_key=' + key
+		};
+
+		var req = http.get(options, function(res)
+		{
+			var pageData = '';
+			res.setEncoding('utf8');
+			res.on('data', function(chunk)
+			{
+				pageData += chunk;
+			});
+			res.on('end', function()
+			{
+				var JSONData = JSON.parse(pageData);
+				if (JSONData.response.posts[0].type === 'photo')
+				{
+					photo.url = JSONData.response.posts[0].photos[0].original_size.url;
+					photo.tags = JSONData.response.posts[0].photos[0].tags;
+					photo.tag = (photo.tags.length > 0 ? photo.tags[0] : '');
+					photo.post_url = JSONData.response.posts[0].photos[0].post_url;
+					photo.timestamp = JSONData.response.posts[0].photos[0].timestamp;
+					photo.data = []; //hi rolando
+					console.log("parsedBig 20");
+					self.emit("parsedBig", photo, 20);
+				}
+				else
+				{
+					self.emit("f_initURL", photo);
+				}
+			});
+		});
+
 		console.log("newURLPhoto");
-		self.emit("newURLPhoto", photo);
-	}
+		//self.emit("newURLPhoto", photo);
+	}*/
 
 	var _pull = function(photo, limit) {	
 		if (typeof photo.tag == "undefined") photo.tag = 'dog';
@@ -26,7 +69,7 @@ var Photo = function() {
 
 		var options = {
 			host: 'api.tumblr.com',
-			path: '/v2/tagged?tag=' + photo.tag + '&api_key=' + key + '&before=' + photo.timestamp + '&limit=' + limit
+			path: '/v2/tagged?tag=' + photo.tag + '&api_key=' + key + '&before=' + (photo.similar.length === 0 ? photo.timestamp: photo.similar[photo.similar.length - 1].timestamp) + '&limit=' + limit
 		};
 		var req = http.get(options, function(res)
 		{
@@ -66,6 +109,7 @@ var Photo = function() {
 			photo.tags = json.tags;
 			photo.post_url = json.post_url;
 			photo.timestamp = json.timestamp;
+			photo.data = []; //hi rolando
 			console.log("parsedBig 20");
 			self.emit("parsedBig", photo, 20);
 		}
@@ -79,20 +123,23 @@ var Photo = function() {
 
 	var _parseSmall = function(photo, data) {
 		var JSONData = JSON.parse(data);
-		for (i = 0; i < JSONData.response.lengh; i++)
+		for (i = 0; i < JSONData.response.length; i++)
 		{
 			if (JSONData.response[i].type === 'photo')
 			{
 				var simpic = {};
 				simpic.tags = JSONData.response[i].tags;
 				simpic.post_url = JSONData.response[i].post_url;
+				if (simpic.post_url === photo.post_url) continue;
 				simpic.timestamp = JSONData.response[i].timestamp;
 				var min = 0;
-				for (j = 1; j < JSONData.response[i].photos.alt_sizes.length; j++)
+				for (j = 1; j < JSONData.response[i].photos[0].alt_sizes.length; j++)
 				{
-					if (JSONData.response[i].photos.alt_sizes[j].width < JSONData.response[i].photos.alt_sizes[j].width) min = j;
+					if (JSONData.response[i].photos[0].alt_sizes[j].width <
+						JSONData.response[i].photos[0].alt_sizes[j].width) min = j;
 				}
-				simpic.url = JSONData.response[i].photos.alt_sizes[min].url;
+				simpic.url = JSONData.response[i].photos[0].alt_sizes[min].url;
+				simpic.data = []; //hi rolando
 				photo.similar[photo.similar.length] = simpic;
 			}
 		}
@@ -111,12 +158,14 @@ var Photo = function() {
 		self.emit("success", photo);
 	}
 
+	self.on("newURLPhoto", self.initTagged);
 	self.on("newTaggedPhoto", _pull);
 	self.on("s_pulledBig", _parseBig);
 	self.on("parsedBig", _pull);
 	self.on("s_pulledSmall", _parseSmall);
 	self.on("parsedSmall", _mosaic);
 	self.on("mosaiced", _success);
+
 }
 
 util.inherits(Photo, events.EventEmitter);
--- tumblr.js
+++ tumblr.js
@@ -2,14 +2,23 @@ var taggedphoto = require("./photo");
 
 function tagged(response, tag) {
 	taggedphoto.initTagged(tag);
-
+	//taggedphoto.initURL('http://sharkinthedungeon.tumblr.com/post/32415676873/its-like-i-broke-up-with-obama-and-hes-not');
+	
 	taggedphoto.on("success", function(taggedphoto)
 	{
 		console.log("This shouldn't have worked... " + taggedphoto);
-		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.writeHead(200, {"Content-Type": "text/plain"});	
 		response.write(JSON.stringify(taggedphoto));
 		response.end();
+		
 	});
+	/*
+	taggedphoto.on("f_initURL", function(taggedphoto)
+	{
+		console.log("lol u fuked up");
+		response.write("This post isn't an image.");
+	});*/
+	
 
 }
 
