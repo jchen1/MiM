ee488d5a42f103d80754edd5fe991708f8cb10e0
Jeff Chen <jeffrey@cmu.edu>
2012-09-29 20:38:37 -0400

    derp i suck at coding

--- tumblr.js
+++ tumblr.js
@@ -3,6 +3,7 @@ var http = require("http");
 
 function pull(response, tag) {
 	if (typeof tag == "undefined") tag = 'dog';
+	var timestamp = 0;
 
 	//Callback because wtf node
 	function httpCallback(res, response)
@@ -27,7 +28,7 @@ function pull(response, tag) {
 
 	var options = {
 		host: 'api.tumblr.com',
-		path: '/v2/tagged?tag=' + tag + '&api_key=' + key
+		path: '/v2/tagged?tag=' + tag + '&api_key=' + key + '&timestamp=' + timestamp
 	};
 
 	var req = http.get(options, function(res)
@@ -44,22 +45,26 @@ function parseJSON(response, data)
 {
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	var JSONData = JSON.parse(data);
+	console.log(JSONData);
 	for (i = 0; i < JSONData.response.length; i++)
 	{
 		if (typeof JSONData.response[i].photos != 'undefined')
 		{
 			var photos = JSONData.response[i].photos;
-			var max = 0;
-			for (j = 1; j < photos.length; j++)
+			var min = 0;
+			if (typeof photos[0].alt_sizes === 'undefined')
 			{
-				if (photos[j].original_size.width > photos[max].original_size.width) max = j;
+				response.write('url of ' + i + ' : ' + photos[0].original_size.url + '\n');
 			}
-			response.write('url of ' + i + ' : ' + photos[max].original_size.url + '\n');
+			for (j = 1; j < photos[0].alt_sizes.length; j++)
+			{
+				if (photos[0].alt_sizes[j].width < photos[0].alt_sizes[min].width) min = j;
+			}
+			response.write('url of ' + i + ' : ' + photos[0].alt_sizes[min].url + '\n');
 		}
 		else response.write(i + ' is not an image\n');
 	}
-	//console.log(JSONData.response[1].photos);
-	//console.log(JSONData);
+	response.write('\nearliest timestamp: ' + JSONData.response[JSONData.response.length - 1].timestamp);
     response.end();
 }
 
