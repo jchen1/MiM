3d41761f84a6fbaa718ceb15d3f6118a2b117aed
Cary Yang <caryy@cmu.edu>
2012-11-21 19:32:15 -0500

    Derp

--- requestHandlers.js
+++
@@ -1,8 +0,0 @@
-function start(response) {
-	console.log("start() called");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello there. You're not in the right place.");
-    response.end();
-}
-
-exports.start = start;
